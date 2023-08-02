d6a3c8a184724e92a223dc01e075e17c5d5b7c2f
Pijus <pijus.stockus@gmail.com>
2023-08-02 17:23:37 +0300

    write tests for Triangle class

---
+++ tests/detection/test_triangle.py
@@ -0,0 +1,23 @@
+from detection.triangle import Triangle
+
+import pytest
+
+@pytest.fixture
+def triangle_instance():
+    triangle = Triangle(0, 50, 50, 10, 30, 30)
+    return triangle
+
+def test_returns_correct_list_of_xs(triangle_instance:Triangle):
+    xs = triangle_instance.get_xs()
+    
+    assert xs == [0, 50]
+
+def test_returns_correct_list_of_high_ys(triangle_instance:Triangle):
+    xs = triangle_instance.get_high_ys()
+    
+    assert xs == [50, 30]
+
+def test_returns_correct_list_of_low_ys(triangle_instance:Triangle):
+    xs = triangle_instance.get_low_ys()
+    
+    assert xs == [10, 30]
