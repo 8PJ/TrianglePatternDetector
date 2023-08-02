ee7526fef740955277b3b82d23fd1714083642c9
Pijus <pijus.stockus@gmail.com>
2023-08-02 19:22:57 +0300

    write tests for triangle detector

--- detection/triangle_detector.py
+++ detection/triangle_detector.py
@@ -2,11 +2,8 @@ from detection.detection_functionality.highs_lows import mark_highs_lows
 from detection.detection_functionality.triangle_check import get_triangle
 
 class TriangleDetector:
-    def __init__():
-        pass
-
     def get_triangle_if_exists(self, price_data, hl_num_before, hl_num_after, max_triangle_len):
-        price_data_with_hl = mark_highs_lows(price_data.price_data, hl_num_before, hl_num_after)
+        price_data_with_hl = mark_highs_lows(price_data.price_data_df, hl_num_before, hl_num_after)
 
         triangle = get_triangle(price_data_with_hl, max_triangle_len)
 
