edf8f14f3835507d2ee3884a931839c059188d0a
Pijus <pijus.stockus@gmail.com>
2023-08-02 15:15:54 +0300

    add checks to PriceData class

--- data/price_data.py
+++ data/price_data.py
@@ -1,6 +1,11 @@
+import pandas as pd
+
 # holds the pandas dataframe containing price data
 class PriceData:
     def __init__(self, ticker):
+        if ticker == None:
+            raise ValueError("ticker must be defined")
+        
         self._ticker = ticker
         self._price_data = None
 
@@ -18,4 +23,7 @@ class PriceData:
     
     @price_data.setter
     def price_data(self, price_df):
-        self._price_data = price_df
+        if isinstance(price_df, pd.DataFrame):
+            self._price_data = price_df
+        else:
+            raise TypeError("price_data must be of type pandas.DataFrame")
